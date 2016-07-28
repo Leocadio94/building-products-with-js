744eb995644c62cb0da5a4782c5247625b6a21b2
Gabriel Leocádio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-07-28 19:58:30 -0300

    move express app into separate file to prepare it for testing

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,11 @@
-// npm packages
-import express from 'express';
-
-// init app
-const app = express();
-
-// test method
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-// catch all unhandler errors
-app.use((err,req,res) => {
-	console.error(err.stack);
-	res.status(500).send(err);
-});
+// our packages
+import app from './app';
 
 // start server
 app.listen(8080, function(){
 	const host = this.address().address;
 	const port = this.address().port;
-	console.log('Shard listening at http://${host}:${port}');
+	console.log(`Shard listening at http://${host}:${port}`);
 });
 
 // output all uncaught exceptions
